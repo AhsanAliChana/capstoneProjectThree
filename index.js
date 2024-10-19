c78b6240e5a10f497db7c9108a497dbddb0d9d6a
ahsanalichanna <ahsanalichanna.xwave@gmail.com>
2024-10-19 03:12:28 -0700

    capstone project compelet

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
 let bgColor = document.getElementById("btn")
 bgColor.style.backgroundColor= "#5FA232";
+
+// form sign up
+function data(){
+
+var a = document.getElementById("n1").value;
+var b = document.getElementById("n2").value;
+var c = document.getElementById("n3").value;
+var d = document.getElementById("n4").value;
+
+if (a == "" || b == "" || c == "" || dd == "")
+{
+    alert("All Field are mandatory");
+    return false;
+}
+
+
+else if(c!=d)
+
+{
+    alert("Please enter same password");
+    return false
+}
+
+else{
+     true;
+}
+}
+
+
+
+
+
+let cartCount = 0;
+const cartCountElement = document.getElementById('cart-count');
+const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
+
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        cartCount++;
+        cartCountElement.textContent = cartCount;
+        alert('Confirm adde to cart!');
+    });
+});
